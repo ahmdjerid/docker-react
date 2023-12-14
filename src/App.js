36afc0e916e7cdafc6a7bf7d4618d016d8f34c48
ahmedjerid <ahmed.jerid.se@gmail.com>
2023-12-14 19:52:36 +0100

    my seceond commit from branch

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This change form my feature branch
+          Learn React
         </a>
+        <p>thanks from my feature branch</p>
       </header>
     </div>
   );
